9e2ed63a50f1c4d04e8c7933dde70087c3ac7e39
gautamrajesh007 <gautamrdev7@gmail.com>
2023-08-22 16:36:01 +0530

    Rearranged the whole directory

---
+++ Back-End/src/main/java/com/test_movies/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.test_movies.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
